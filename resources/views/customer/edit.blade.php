36ab84cc5c86a0a866849594a8af1a9d569eb5df
Harsh Madhav <harsh.hm1996@gmail.com>
2020-06-28 05:08:41 +0530

    Initial Commit

---
+++ resources/views/customer/edit.blade.php
@@ -0,0 +1,11 @@
+<h1>Edit Customer Details</h1>
+
+<form action="/customers/{{ $customer->id }}" method="post">
+
+
+     @method('PATCH')
+
+@include('customer.form')
+    <button>Save Customer</button>
+
+</form>
